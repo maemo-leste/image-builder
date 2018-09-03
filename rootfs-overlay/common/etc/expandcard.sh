fec9ffa4ea6b89ffdbc247e19f76489c61a92db8
Sicelo <absicsz@gmail.com>
2018-09-03 05:55:51 +0200

    Fix expandcard.sh script

--- rootfs-overlay/common/etc/expandcard.sh
+++ rootfs-overlay/common/etc/expandcard.sh
@@ -4,26 +4,27 @@
 # This file is part of Maemo Leste.
 #
 
-[ "$UID" = 0 ] || {
+[ $( id -u ) = 0 ] || {
 	echo "This script should be ran as root!"
 	exit 1
 }
 
-root="$(egrep -o 'root[^ ]*' /proc/cmdline | sed 's/root=//')"
+root="$(egrep -o 'root=[^ ]*' /proc/cmdline | sed 's/root=//')"
 card="$(lsblk -no PKNAME $root)"
+start="$(partx -g -s -o START $root)"
 
 # TODO: The '|| true' is because sometimes after fdisk an ioctl fails.
-fdisk "$card" <<EOF || true
+fdisk "/dev/$card" <<EOF || true
 d
 2
 n
 p
 2
+$start
 
 
-n
 w
 EOF
 
-partprobe "$card"
+partprobe "/dev/$card"
 resize2fs "$root"
